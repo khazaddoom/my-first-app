b43538a75da55c4e67ccaeee2fbbb33e0ce4680e
unknown <31121986g@gmail.com>
2019-06-03 17:50:24 +0530

    creating new component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SecondComponent} from './second.component';
 
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SecondComponent
   ],
   imports: [
     BrowserModule, FormsModule
